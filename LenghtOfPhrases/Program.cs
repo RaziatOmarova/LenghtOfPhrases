2c94e1d748af447e4be14f6131b70947fcd38c4e
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-07 22:26:30 +0300

    Добавьте файлы проекта.

---
+++ LenghtOfPhrases/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenghtOfPhrases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Sake = new string[5] {"Екатерина", "Виктория", "Виктор", "Андрей", "Аня"};
+            Console.WriteLine(Name(Sake));
+            Console.ReadKey();
+        }
+        public static int Name(string[] number)
+        {
+            int n = 0;
+            for(int i = 0; i < number.Length; i++)
+            {
+                if (number[i].Length >= 5)
+                {
+                    n++;
+                }
+            }
+            return n;
+        }
+    }
+}
